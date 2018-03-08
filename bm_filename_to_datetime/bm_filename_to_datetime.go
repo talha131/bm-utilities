14afda560ce69d7a1d8b3b31b60f3ecc63efc44f
Talha Mansoor <talha131@gmail.com>
2018-03-08 23:40:30 +0500

    Add comments

--- bm_filename_to_datetime/bm_filename_to_datetime.go
+++ bm_filename_to_datetime/bm_filename_to_datetime.go
@@ -12,16 +12,24 @@ func main() {
 	for i := 1; i < len(os.Args); i++ {
 		file := os.Args[i]
 		ext := strings.ToLower(filepath.Ext(file))
+
+		// Get file stats
 		info, err := os.Stat(file)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			continue
 		}
+
+		// Skip if it is a directory
 		if info.IsDir() {
 			continue
 		}
+
+		// Create new name
 		time := info.ModTime().Format("2006-01-02 150405")
 		newName := time + ext
+
+		// Rename
 		err = os.Rename(file, newName)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
