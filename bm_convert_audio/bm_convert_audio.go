668cf9a91af6a2ba660430a3b78fa78d90a61a8f
Talha Mansoor <talha131@gmail.com>
2018-03-09 00:58:36 +0500

    Refactor code and process file via ffmpeg

--- bm_convert_audio/bm_convert_audio.go
+++ bm_convert_audio/bm_convert_audio.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"mime"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -12,35 +13,51 @@ import (
 func main() {
 
 	isVerbose := flag.Bool("v", false, "verbose")
-	// outputFormat := flag.String("o", "wav", "output format. wav|mp3")
-	mimeType := "audio/mpeg"
+	outputFormat := flag.String("o", "wav", "output format. wav|mp3")
 	flag.Parse()
+
+	cmd := "ffmpeg"
+	cmdArgs := "-ac 1 -ab 64k -ar 44100"
 
 	for i := 0; i < len(flag.Args()); i++ {
 		file := flag.Args()[i]
 
-		// Get file stats
-		fileInfo, err := os.Stat(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-
-		// Skip if it is a directory
-		if fileInfo.IsDir() {
-			if *isVerbose {
-				fmt.Printf("Skipping %v\n", fileInfo.Name())
+		if isFileAudio(file, *isVerbose) {
+			name := strings.TrimSuffix(file, filepath.Ext(file))
+			output := name + "." + *outputFormat
+			_, err := exec.Command(cmd, "-i", file, cmdArgs, output).Output()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
 			}
-			continue
-		}
-
-		ext := strings.ToLower(filepath.Ext(file))
-		fileType := mime.TypeByExtension(ext)
-		if fileType != mimeType {
-			if *isVerbose {
-				fmt.Printf("Skipping %v \t %v\n", fileInfo.Name(), fileType)
-			}
-			continue
 		}
 	}
+}
+
+func isFileAudio(file string, isVerbose bool) bool {
+	mimeType := "audio/mpeg"
+	// Get file stats
+	fileInfo, err := os.Stat(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return false
+	}
+
+	if fileInfo.IsDir() {
+		if isVerbose {
+			fmt.Printf("Skipping %v\n", fileInfo.Name())
+		}
+		return false
+	}
+
+	ext := strings.ToLower(filepath.Ext(file))
+	fileType := mime.TypeByExtension(ext)
+	if fileType != mimeType {
+		if isVerbose {
+			fmt.Printf("Skipping %v \t %v\n", fileInfo.Name(), fileType)
+		}
+		return false
+	}
+
+	return true
 }
