e1f788880b77fcdd3e27782c77227d6678d13a53
Talha Mansoor <talha131@gmail.com>
2018-03-12 09:11:05 +0500

    Remove cobra auto-generated comments

--- cmd/rename.go
+++ cmd/rename.go
@@ -54,16 +54,6 @@ var renameCmd = &cobra.Command{
 
 func init() {
 	fileCmd.AddCommand(renameCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// renameCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// renameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func rename(file string, newName string) {
