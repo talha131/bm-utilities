d49c5f6a46d6d466f7c6f78f0232bdab783d0e94
Talha Mansoor <talha131@gmail.com>
2018-03-18 22:46:36 +0500

    Add code to create video loop. Does not work.

--- cmd/videoLoop.go
+++ cmd/videoLoop.go
@@ -22,7 +22,10 @@ package cmd
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -35,10 +38,26 @@ var videoLoopCmd = &cobra.Command{
 It picks only the video stream and discards audio stream.
 Output format is mp4.
 
--c and -d are mutually exclusive.
+-c and -d are mutually exclusive. -c has precendence over -d.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("videoLoop called")
+		count, errC := cmd.Flags().GetUint16("count")
+		duration, errD := cmd.Flags().GetUint16("duration")
+
+		if errC != nil && errD != nil {
+			fmt.Fprintf(os.Stderr, "Unable to find Count or Duration. At least one is required")
+			return
+		}
+
+		oPath := createOutputDirectory(cmd)
+
+		fmt.Println(count, duration)
+		if duration == 0 && errC == nil && count > 0 {
+			processVideoLoop(count, oPath, args)
+		} else if errD == nil && duration > 0 {
+			processVideoLoop(duration, oPath, args)
+		}
+
 	},
 }
 
@@ -48,4 +67,54 @@ func init() {
 	videoLoopCmd.Flags().Uint16P("count", "c", 3, "Number of times to concatenate the video")
 	videoLoopCmd.Flags().Uint16P("duration", "d", 0, "Minimum minutes of the video")
 	videoLoopCmd.Flags().StringP("outputDirectory", "o", "", "Output directory path. Default is current.")
+}
+
+func getFilterComplexParam(count uint16) string {
+
+	p := fmt.Sprintf("%s \"%sconcat=n=%d:v=1[outv]\" -map \"[outv]\"",
+		"-filter_complex",
+		strings.Repeat("[0:v:0]", int(count)),
+		count)
+
+	if v, _ := rootCmd.Flags().GetBool("verbose"); v {
+		fmt.Printf("filter_complex is\n%s\n", p)
+	}
+
+	return p
+}
+
+func getOutputFileName(oPath string, f string, suffix string) string {
+	fn := getFileNameWithoutExtension(f) + "_" + suffix + "." + "mp4"
+
+	return filepath.Join(oPath, fn)
+}
+
+func processVideoLoop(count uint16, oPath string, args []string) {
+	for _, e := range args {
+		fmt.Println(e)
+		if isFileVideo(e) {
+			createVideoLoop(count,
+				e,
+				getOutputFileName(oPath, e, fmt.Sprintf("%s-%d", "loop", count)))
+		}
+	}
+}
+
+func createVideoLoop(count uint16, file string, output string) {
+	filter := getFilterComplexParam(count)
+	var input []string
+	input = append(input, "-i", file, filter, output)
+
+	if v, _ := rootCmd.Flags().GetBool("verbose"); v {
+		fmt.Printf("Command is\n%s\n", input)
+	}
+
+	cmd := exec.Command(app, input...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
